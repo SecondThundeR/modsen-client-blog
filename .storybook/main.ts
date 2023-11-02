4663497624b650f02dadc9dd880a90818c0751a4
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-02 22:01:26 +0300

    feat(.storybook): add Google fonts support

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,5 +14,11 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  previewHead: (head) => `
+    ${head}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;900&family=Roboto+Serif:opsz,wght@8..144,300;8..144,400;8..144,500;8..144,600;8..144,700;8..144,800&family=Sen:wght@700&display=swap" rel="stylesheet">
+  `,
 };
 export default config;

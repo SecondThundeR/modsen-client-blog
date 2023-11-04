394c4d01f42448f817a0c31c31fa08a56b7b9e4d
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-04 13:53:40 +0300

    fix(lib): flip fonts variable

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,13 +3,13 @@ import { Inter, Sen } from "next/font/google";
 export const inter = Inter({
   subsets: ["latin"],
   weight: ["400", "600", "700", "900"],
-  variable: "--sen-font",
+  variable: "--inter-font",
   display: "swap",
 });
 
 export const sen = Sen({
   subsets: ["latin"],
   weight: ["700"],
-  variable: "--inter-font",
+  variable: "--sen-font",
   display: "swap",
 });

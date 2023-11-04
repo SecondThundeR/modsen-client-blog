e0c9703f343f3f79490fd344a527349c4b27e1d7
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-04 16:36:46 +0300

    refactor(app): update main page
    Update page params type
    Remove `Title` components from showcase

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,26 +1,15 @@
-import { Button, Display, Title } from "@/components/ui";
+import { Button, Display } from "@/components/ui";
 import { getDictionary } from "@/locale/get-dictionary";
-import { type Locale } from "@/locale/i18n-config";
 
 import LocaleSwitcher from "./_components/LocaleSwitcher/LocaleSwitcher";
 
-export default async function Home({
-  params: { locale },
-}: {
-  params: { locale: Locale };
-}) {
+export default async function Home({ params: { locale } }: PageLocaleParams) {
   const dictionary = await getDictionary(locale);
 
   return (
     <main>
       <LocaleSwitcher />
       <Display>{dictionary.hello}</Display>
-      <Title headingLevel={1}>Heading 1</Title>
-      <Title headingLevel={2}>Heading 2</Title>
-      <Title headingLevel={3}>Heading 3</Title>
-      <Title headingLevel={4}>Heading 4</Title>
-      <Title headingLevel={5}>Heading 5</Title>
-      <Title headingLevel={6}>Heading 6</Title>
       <Button>{dictionary.button}</Button>
     </main>
   );

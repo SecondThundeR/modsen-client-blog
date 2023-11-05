2d877bd0b81e8a7f6b30c3950a583325661e1111
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-05 18:01:12 +0300

    feat(components): add `ErrorWrapper` component

---
+++ src/components/ui/ErrorWrapper/ErrorWrapper.tsx
@@ -0,0 +1,10 @@
+import styles from "./ErrorWrapper.module.scss";
+import { type ErrorWrapperProps } from "./interfaces";
+
+export default function ErrorWrapper({ children }: ErrorWrapperProps) {
+  return (
+    <div data-testid="error-wrapper" className={styles.error}>
+      {children}
+    </div>
+  );
+}
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,7 @@
 export { default as Body } from "./Body/Body";
 export { default as Button } from "./Button/Button";
 export { default as Display } from "./Display/Display";
+export { default as ErrorWrapper } from "./ErrorWrapper/ErrorWrapper";
 export { default as Footer } from "./Footer/Footer";
 export { default as Header } from "./Header/Header";
 export { default as Heading } from "./Heading/Heading";

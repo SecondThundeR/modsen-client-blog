49be83094e457ce78858b33dcfd10d978f360d55
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-07 01:15:59 +0300

    feat: update modal logic
    > Now modal depends on current search params and to show certain modal, we need to provide `?modal=content-id-...` to `Link` href property
    
    Add `ModalWrapper`
    Add `modalData` constant (Map for modal search param and related React node)
    Move `Modal` from `ui/` folder
    Rename `Modal` to `ModalShell`

---
+++ src/constants/modalData.tsx
@@ -0,0 +1,13 @@
+import { type ReactNode } from "react";
+
+export const modalData: Record<string, ReactNode> = {
+  "about-us-video": (
+    <iframe
+      width="1280"
+      height="720"
+      src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?si=TWl5YT9BtQX7sPsD"
+      title="YouTube video player"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    ></iframe>
+  ),
+};

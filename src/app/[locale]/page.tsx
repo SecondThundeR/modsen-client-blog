2e709f82f6e304a2c2849ad32877897a9a1c4d60
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-08 18:50:43 +0300

    fix(app): remove dummy elements for locale demo

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,9 @@
-import { Button, Display } from "@/components/ui";
-import { getDictionary } from "@/locale/get-dictionary";
-
 import LocaleSwitcher from "./_components/LocaleSwitcher/LocaleSwitcher";
 
-export default async function Home({ params: { locale } }: PageLocaleParams) {
-  const dictionary = await getDictionary(locale);
-
+export default function Home() {
   return (
     <main>
       <LocaleSwitcher />
-      <Display>{dictionary.hello}</Display>
-      <Button>{dictionary.button}</Button>
     </main>
   );
 }

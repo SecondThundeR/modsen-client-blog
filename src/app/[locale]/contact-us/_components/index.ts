7b3fa90f3087b0b36854612ee3765a0588802900
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-08 18:20:31 +0300

    feat(app): add `ContactUsMap` component
    This client component displays map with three location points, defined in constants

--- src/app/[locale]/contact-us/_components/index.ts
+++ src/app/[locale]/contact-us/_components/index.ts
@@ -1,4 +1,5 @@
 export { default as ContactForm } from "./ContactForm/ContactForm";
 export { default as ContactFormAlert } from "./ContactFormAlert/ContactFormAlert";
 export { default as ContactUsBlock } from "./ContactUsBlock/ContactUsBlock";
+export { default as ContactUsMap } from "./ContactUsMap/ContactUsMap";
 export { default as GeneralWrapper } from "./GeneralWrapper/GeneralWrapper";

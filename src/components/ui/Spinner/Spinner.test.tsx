fca482c83a6460f9952402fa8362f2dd5b40f00b
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-08 21:42:55 +0300

    feat(components): add `Spinner` component

---
+++ src/components/ui/Spinner/Spinner.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+
+import Spinner from "./Spinner";
+
+describe("Spinner", () => {
+  test("should render spinner with white color by default", () => {
+    render(<Spinner />);
+
+    const spinner = screen.getByTestId("spinner");
+
+    expect(spinner).toBeInTheDocument();
+    expect(spinner.nodeName.toLowerCase()).toEqual("span");
+    expect(spinner.className).toEqual("spinner spinner__white");
+  });
+
+  test("should render spinner with black color", () => {
+    render(<Spinner color="black" />);
+
+    const spinner = screen.getByTestId("spinner");
+
+    expect(spinner).toBeInTheDocument();
+    expect(spinner.nodeName.toLowerCase()).toEqual("span");
+    expect(spinner.className).toEqual("spinner spinner__black");
+  });
+});

5aef7307bd87db59c171d0f756101186842515d8
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-13 19:00:30 +0300

    feat(components): update `PageSection`
    Add new props: `align` (controls flexbox align), `gap` (controls flexbox gap), `sidePaddings` (controls size of side paddings, when `fullWidth` is enabled)

--- src/components/ui/PageSection/PageSection.test.tsx
+++ src/components/ui/PageSection/PageSection.test.tsx
@@ -16,7 +16,9 @@ describe("PageSection", () => {
 
     expect(sectionContent).toBeInTheDocument();
     expect(pageSection.nodeName.toLowerCase()).toEqual("section");
-    expect(pageSection.className).toEqual("wrapper");
+    expect(pageSection.className).toEqual(
+      "wrapper wrapper__center gap__regular",
+    );
   });
 
   test("should render page section with full width", () => {
@@ -29,6 +31,64 @@ describe("PageSection", () => {
     const pageSection = screen.getByTestId("page-section");
 
     expect(pageSection.nodeName.toLowerCase()).toEqual("section");
-    expect(pageSection.className).toEqual("wrapper fullWidth");
+    expect(pageSection.className).toEqual(
+      "wrapper wrapper__center gap__regular fullWidth__regular",
+    );
+  });
+
+  test("should render page section with full width and large side paddings", () => {
+    render(
+      <PageSection fullWidth sidePaddings="large">
+        <p>Content</p>
+      </PageSection>,
+    );
+
+    const pageSection = screen.getByTestId("page-section");
+
+    expect(pageSection.nodeName.toLowerCase()).toEqual("section");
+    expect(pageSection.className).toEqual(
+      "wrapper wrapper__center gap__regular fullWidth__large",
+    );
+  });
+
+  test("should render page section with start align", () => {
+    render(
+      <PageSection align="start">
+        <p>Content</p>
+      </PageSection>,
+    );
+
+    const pageSection = screen.getByTestId("page-section");
+
+    expect(pageSection.nodeName.toLowerCase()).toEqual("section");
+    expect(pageSection.className).toEqual(
+      "wrapper wrapper__start gap__regular",
+    );
+  });
+
+  test("should render page section with end align", () => {
+    render(
+      <PageSection align="end">
+        <p>Content</p>
+      </PageSection>,
+    );
+
+    const pageSection = screen.getByTestId("page-section");
+
+    expect(pageSection.nodeName.toLowerCase()).toEqual("section");
+    expect(pageSection.className).toEqual("wrapper wrapper__end gap__regular");
+  });
+
+  test("should render page section with small gap", () => {
+    render(
+      <PageSection gap="small">
+        <p>Content</p>
+      </PageSection>,
+    );
+
+    const pageSection = screen.getByTestId("page-section");
+
+    expect(pageSection.nodeName.toLowerCase()).toEqual("section");
+    expect(pageSection.className).toEqual("wrapper wrapper__center gap__small");
   });
 });
--- src/components/ui/PageSection/PageSection.tsx
+++ src/components/ui/PageSection/PageSection.tsx
@@ -6,13 +6,21 @@ import styles from "./PageSection.module.scss";
 export default function PageSection({
   children,
   fullWidth = false,
+  align = "center",
+  sidePaddings = "regular",
+  gap = "regular",
 }: PageSectionProps) {
   return (
     <section
       data-testid="page-section"
-      className={clsx(styles.wrapper, {
-        [styles.fullWidth!]: fullWidth,
-      })}
+      className={clsx(
+        styles.wrapper,
+        styles[`wrapper__${align}`],
+        styles[`gap__${gap}`],
+        {
+          [styles[`fullWidth__${sidePaddings}`]!]: fullWidth,
+        },
+      )}
     >
       {children}
     </section>

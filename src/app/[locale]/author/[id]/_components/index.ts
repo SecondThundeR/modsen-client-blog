29c96ba5a4265078c63f4fae21dbc5e352112db0
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-13 19:01:23 +0300

    feat: add support for `Author` page

---
+++ src/app/[locale]/author/[id]/_components/index.ts
@@ -0,0 +1,3 @@
+export { default as AuthorHeader } from "./AuthorHeader/AuthorHeader";
+export { default as Posts } from "./Posts/Posts";
+export { default as PostsHeading } from "./PostsHeading/PostsHeading";
--- src/app/[locale]/author/[id]/page.tsx
+++ src/app/[locale]/author/[id]/page.tsx
@@ -1,4 +1,12 @@
+import { redirect } from "next/navigation";
+
+import { PageSection } from "@/components/ui";
 import { authors } from "@/constants/data/authors";
+import { routes } from "@/constants/routes";
+import { findAuthorByID } from "@/lib/authors/findAuthorByID";
+import { getDictionary } from "@/locale/get-dictionary";
+
+import { AuthorHeader, Posts, PostsHeading } from "./_components";
 
 export function generateStaticParams() {
   return authors.map((author) => ({
@@ -6,10 +14,34 @@ export function generateStaticParams() {
   }));
 }
 
-export default function Author({ params: { id } }: { params: { id: string } }) {
+type AuthorPageParams = PageLocaleParams & { params: { id: string } };
+
+export default async function Author({
+  params: { id, locale },
+}: AuthorPageParams) {
+  const authorData = findAuthorByID(id);
+  if (!authorData) {
+    redirect(`/${locale}${routes.home}`);
+  }
+
+  const dictionary = await getDictionary(locale);
+  const {
+    categoryGrid: { categories },
+    authorPage: { header, postsHeading, noPostsHeading },
+  } = dictionary;
+
   return (
     <main>
-      <h1>Author ID: {id}</h1>
+      <AuthorHeader authorData={authorData} dictionary={header} />
+      <PageSection fullWidth gap="small" sidePaddings="large" align="start">
+        <PostsHeading headingString={postsHeading} />
+        <Posts
+          locale={locale}
+          authorId={authorData.id}
+          categoriesDictionary={categories}
+          noPostsHeading={noPostsHeading}
+        />
+      </PageSection>
     </main>
   );
 }

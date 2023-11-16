e716dbd5d1759df203c19baf4b4ee198fa32177f
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-16 15:56:17 +0300

    fix: check for category existance using another helper

--- src/lib/posts/getPostsByCategoryID.ts
+++ src/lib/posts/getPostsByCategoryID.ts
@@ -1,8 +1,5 @@
-import { categories } from "@/constants/data/categories";
 import { posts } from "@/constants/data/posts";
 
 export function getPostsByCategoryID(categoryId: string) {
-  if (categories.filter(({ id }) => id === categoryId).length === 0)
-    return null;
   return posts.filter((post) => post.categoryId === categoryId);
 }

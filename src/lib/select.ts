e1580741d6b10bb7301c5559bf6aaa2f7f07007a
SecondThundeR <awayfromgalaxy@gmail.com>
2023-11-17 10:50:58 +0300

    fix(lib): correct generic type for `isSelectedOptionDisabled`

--- src/lib/select.ts
+++ src/lib/select.ts
@@ -1,6 +1,6 @@
 export function isSelectedOptionDisabled<
-  T extends { isDisabled?: boolean; value?: string }[],
->(options: T, value: string) {
+  T extends { isDisabled?: boolean; value?: string },
+>(options: T[], value: string) {
   const disabledValues = options
     .filter((option) => option.isDisabled)
     .map((option) => option.value);
